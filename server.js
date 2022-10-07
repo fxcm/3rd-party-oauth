579684ae06d17bab0705b3d41ca9b8ceb9c5861a
Andre Mermegas <amermegas@fxcm.com>
2022-10-07 09:16:30 -0400

    example code updates for oauth2

--- server.js
+++ server.js
@@ -1,21 +1,15 @@
-var fs = require('fs');
-var path = require('path');
-var axios = require('axios');
-var randomstring = require('randomstring');
-var querystring = require('querystring');
-var session = require('express-session');
-var express = require('express');
-var app = express();
+const fs = require('fs');
+const path = require('path');
+const axios = require('axios');
+const randomstring = require('randomstring');
+const querystring = require('querystring');
+const session = require('express-session');
+const express = require('express');
+const app = express();
 
 
 // Load configuration
-var CONFIG = JSON.parse(fs.readFileSync('./config.json', { encoding: 'utf8' }));
-
-
-function encodeClientCredentials(clientId, clientSecret) {
-	return new Buffer(querystring.escape(clientId) + ':' + querystring.escape(clientSecret)).toString('base64');
-};
-
+const CONFIG = JSON.parse(fs.readFileSync('./config.json', {encoding: 'utf8'}));
 
 // Set express middleware
 app.set('view engine', 'ejs');
@@ -27,44 +21,43 @@ app.use(session({
     saveUninitialized: true
 }));
 
-app.get('/config', function(req, res) {
+app.get('/config', function (req, res) {
     res.json({
         client_id: CONFIG.client_id,
         oauth_url: CONFIG.oauth_url
     });
 });
 
-app.get('/authorize', function(req, res) {
-    var state = randomstring.generate();
-    var url = new URL(CONFIG.oauth_url);
-    var clientId = CONFIG.client_id;
-    var redirectURL = CONFIG.redirect_url;
 
-    url.pathname = '/oauth/authenticate';
+app.get('/authorize', function (req, res) {
+    let state = randomstring.generate();
+    const url = new URL(CONFIG.oauth_url);
+    const clientId = CONFIG.client_id;
+    const redirectURL = CONFIG.redirect_url;
+
+    url.pathname = '/oauth2/authorize';
     url.searchParams.set('response_type', 'code');
+    url.searchParams.set('scope', 'openid trading');
     url.searchParams.set('client_id', clientId);
     url.searchParams.set('redirect_uri', redirectURL);
     url.searchParams.set('state', state);
-    
 
-    // temp cache state
     req.session.state = state;
-
     res.redirect(url.toString());
 });
 
 
-app.get('/callback', function(req, res) {
+app.get('/authorized', function (req, res) {
     // Handle error responses
     if (req.query.error) {
         console.log(req.query.error);
-        res.json({ error: req.query.error })
+        res.json({error: req.query.error})
         return;
     }
 
     // State values from query and session
-    var responseState = req.query.state;
-    var sessionState = req.session.state;
+    const responseState = req.query.state;
+    const sessionState = req.session.state;
 
     // Delete temporary state
     delete req.session.state;
@@ -72,52 +65,71 @@ app.get('/callback', function(req, res) {
     // Validate state
     if (responseState !== sessionState) {
         console.log(`State DOES NOT MATCH: expected ${sessionState} got ${responseState}`);
-		res.json({error: 'State value did not match'});
-		return;
+        res.json({error: 'State value did not match'});
+        return;
     }
 
     // Exchange auth code for access token through back channel POST, authorization_grant flow
-    var tokenURL = CONFIG.oauth_url + '/oauth/token';
-    var clientId = CONFIG.client_id;
-    var clientSecret = CONFIG.client_secret;
-    var headers = {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': 'Basic ' + encodeClientCredentials(clientId, clientSecret)
-    }; 
-    var postBody = {
+    const tokenURL = CONFIG.oauth_url + '/oauth2/token';
+    let postBody = {
+        grant_type: 'authorization_code',
+        redirect_uri: CONFIG.redirect_url,
         code: req.query.code,
-        grant_type: 'authorization_code'
+        client_id: CONFIG.client_id,
+        client_secret: CONFIG.client_secret
     };
 
     axios.request({
         method: 'POST',
         url: tokenURL,
-        headers: headers,
         data: querystring.stringify(postBody)
-    })
-    .then(function(result) {
+    }).then(function (result) {
         console.log(result.data);
-        var accessToken = result.data.access_token;
-        req.session.access_token = accessToken;
-        res.redirect('/');  
-    })
-    .catch(function(err) {
-        res.json({ error: err })
+        req.session.access_token = result.data.access_token;
+        req.session.refresh_token = result.data.refresh_token;
+        res.redirect('/');
+    }).catch(function (err) {
+        res.json({error: err})
     });
 });
 
 
-app.get('/logout', function(req, res) {
-    req.session.destroy(function() {
+app.get('/logout', function (req, res) {
+    req.session.destroy(function () {
         res.redirect('/')
     });
 });
 
-app.get('/', function(req, res) {
-    res.render('index', { access_token: req.session.access_token  });
+app.get('/refresh', function (req, res) {
+    // Exchange auth code for access token through back channel POST, authorization_grant flow
+    const tokenURL = CONFIG.oauth_url + '/oauth2/token';
+    let postBody = {
+        grant_type: 'refresh_token',
+        redirect_uri: CONFIG.redirect_url,
+        refresh_token: req.session.refresh_token,
+        client_id: CONFIG.client_id,
+        client_secret: CONFIG.client_secret
+    };
+
+    axios.request({
+        method: 'POST',
+        url: tokenURL,
+        data: querystring.stringify(postBody)
+    }).then(function (result) {
+        console.log(result.data);
+        req.session.access_token = result.data.access_token;
+        req.session.refresh_token = result.data.refresh_token;
+        res.redirect('/');
+    }).catch(function (err) {
+        res.json({error: err})
+    });
+});
+
+app.get('/', function (req, res) {
+    res.render('index', {access_token: req.session.access_token, refresh_token: req.session.refresh_token});
 });
 
 // Start server
-app.listen(CONFIG.port, function() {
+app.listen(CONFIG.port, function () {
     console.log(`Server started on port:${CONFIG.port}`)
 });
